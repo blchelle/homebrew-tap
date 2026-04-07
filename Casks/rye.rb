d5221aee8214a99d3f6372cc5de3571783c8401f
Brock Chelle <brocklchelle@gmail.com>
2026-04-07 07:15:51 -0600

    Add rye cask

---
+++ Casks/rye.rb
@@ -0,0 +1,16 @@
+cask "rye" do
+  version "0.0.2"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  url "https://github.com/blchelle/rye/releases/download/v#{version}/Rye-#{version}-arm64.dmg"
+  name "Rye"
+  desc "Eye rest reminder app"
+  homepage "https://github.com/blchelle/rye"
+
+  app "Rye.app"
+
+  zap trash: [
+    "~/Library/Application Support/rye",
+    "~/Library/Preferences/com.rye.eyerest.plist",
+  ]
+end
